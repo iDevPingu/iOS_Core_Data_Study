9bf3975253ebadf9b25b9c33503a884ee8c35d09
pingu <jaicoco8@gmail.com>
2021-06-19 12:03:41 +0900

    Ch02 NSManagedObject Subclasses

---
+++ Ch02/BowTies/BowTies/BowTie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  BowTie+CoreDataClass.swift
+//  BowTies
+//
+//  Created by Ick on 2021/06/19.
+//  Copyright © 2021 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(BowTie)
+public class BowTie: NSManagedObject {
+
+}
